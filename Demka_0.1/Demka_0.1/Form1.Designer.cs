91ef5b92a11b7d2fcedb783d4bb31ffb3b6d3e60
RomanKH123 <114310323+RomanKH123@users.noreply.github.com>
2023-04-18 15:42:04 +0300

    Dem

---
+++ Demka_0.1/Demka_0.1/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Demka_0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.номерDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.видРаботыDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.описаниеРаботыDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.avtoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet11 = new Demka_0._1.DataSet1();
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.oleDbSelectCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbInsertCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbUpdateCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDeleteCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDataAdapter1 = new System.Data.OleDb.OleDbDataAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.номерDataGridViewTextBoxColumn,
+            this.видРаботыDataGridViewTextBoxColumn,
+            this.описаниеРаботыDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.avtoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 426);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // номерDataGridViewTextBoxColumn
+            // 
+            this.номерDataGridViewTextBoxColumn.DataPropertyName = "Номер";
+            this.номерDataGridViewTextBoxColumn.HeaderText = "Номер";
+            this.номерDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.номерDataGridViewTextBoxColumn.Name = "номерDataGridViewTextBoxColumn";
+            this.номерDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.номерDataGridViewTextBoxColumn.Width = 81;
+            // 
+            // видРаботыDataGridViewTextBoxColumn
+            // 
+            this.видРаботыDataGridViewTextBoxColumn.DataPropertyName = "Вид работы";
+            this.видРаботыDataGridViewTextBoxColumn.HeaderText = "Вид работы";
+            this.видРаботыDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.видРаботыDataGridViewTextBoxColumn.Name = "видРаботыDataGridViewTextBoxColumn";
+            this.видРаботыDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // описаниеРаботыDataGridViewTextBoxColumn
+            // 
+            this.описаниеРаботыDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.описаниеРаботыDataGridViewTextBoxColumn.DataPropertyName = "Описание работы";
+            this.описаниеРаботыDataGridViewTextBoxColumn.HeaderText = "Описание работы";
+            this.описаниеРаботыDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.описаниеРаботыDataGridViewTextBoxColumn.Name = "описаниеРаботыDataGridViewTextBoxColumn";
+            this.описаниеРаботыDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // avtoBindingSource
+            // 
+            this.avtoBindingSource.DataMember = "Avto";
+            this.avtoBindingSource.DataSource = this.dataSet11;
+            // 
+            // dataSet11
+            // 
+            this.dataSet11.DataSetName = "DataSet1";
+            this.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\tyi\\Demka_0.1\\Avto.mdb";
+            // 
+            // oleDbSelectCommand1
+            // 
+            this.oleDbSelectCommand1.CommandText = "SELECT        Avto.*\r\nFROM            Avto";
+            this.oleDbSelectCommand1.Connection = this.oleDbConnection1;
+            // 
+            // oleDbInsertCommand1
+            // 
+            this.oleDbInsertCommand1.CommandText = "INSERT INTO `Avto` (`Номер`, `Вид работы`, `Описание работы`) VALUES (?, ?, ?)";
+            this.oleDbInsertCommand1.Connection = this.oleDbConnection1;
+            this.oleDbInsertCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Номер", System.Data.OleDb.OleDbType.VarWChar, 0, "Номер"),
+            new System.Data.OleDb.OleDbParameter("Вид_работы", System.Data.OleDb.OleDbType.VarWChar, 0, "Вид работы"),
+            new System.Data.OleDb.OleDbParameter("Описание_работы", System.Data.OleDb.OleDbType.VarWChar, 0, "Описание работы")});
+            // 
+            // oleDbUpdateCommand1
+            // 
+            this.oleDbUpdateCommand1.CommandText = resources.GetString("oleDbUpdateCommand1.CommandText");
+            this.oleDbUpdateCommand1.Connection = this.oleDbConnection1;
+            this.oleDbUpdateCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Номер", System.Data.OleDb.OleDbType.VarWChar, 0, "Номер"),
+            new System.Data.OleDb.OleDbParameter("Вид_работы", System.Data.OleDb.OleDbType.VarWChar, 0, "Вид работы"),
+            new System.Data.OleDb.OleDbParameter("Описание_работы", System.Data.OleDb.OleDbType.VarWChar, 0, "Описание работы"),
+            new System.Data.OleDb.OleDbParameter("Original_Номер", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Номер", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Вид_работы", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Вид работы", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Вид_работы", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Вид работы", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Описание_работы", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Описание работы", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Описание_работы", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Описание работы", System.Data.DataRowVersion.Original, null)});
+            // 
+            // oleDbDeleteCommand1
+            // 
+            this.oleDbDeleteCommand1.CommandText = "DELETE FROM `Avto` WHERE ((`Номер` = ?) AND ((? = 1 AND `Вид работы` IS NULL) OR " +
+    "(`Вид работы` = ?)) AND ((? = 1 AND `Описание работы` IS NULL) OR (`Описание раб" +
+    "оты` = ?)))";
+            this.oleDbDeleteCommand1.Connection = this.oleDbConnection1;
+            this.oleDbDeleteCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Original_Номер", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Номер", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Вид_работы", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Вид работы", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Вид_работы", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Вид работы", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Описание_работы", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Описание работы", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Описание_работы", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Описание работы", System.Data.DataRowVersion.Original, null)});
+            // 
+            // oleDbDataAdapter1
+            // 
+            this.oleDbDataAdapter1.DeleteCommand = this.oleDbDeleteCommand1;
+            this.oleDbDataAdapter1.InsertCommand = this.oleDbInsertCommand1;
+            this.oleDbDataAdapter1.SelectCommand = this.oleDbSelectCommand1;
+            this.oleDbDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Avto", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("Номер", "Номер"),
+                        new System.Data.Common.DataColumnMapping("Вид работы", "Вид работы"),
+                        new System.Data.Common.DataColumnMapping("Описание работы", "Описание работы")})});
+            this.oleDbDataAdapter1.UpdateCommand = this.oleDbUpdateCommand1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(1010, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(741, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Номер";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1102, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Описание работы";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(889, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Вид работы";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(798, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox2.Location = new System.Drawing.Point(961, 201);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox3.Location = new System.Drawing.Point(1205, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Location = new System.Drawing.Point(1010, 359);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(922, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 28);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(798, 120);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button4.Location = new System.Drawing.Point(1010, 320);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 34);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Изменить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1398, 603);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Списиок 0.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource avtoBindingSource;
+        private DataSet1 dataSet11;
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+        private System.Data.OleDb.OleDbCommand oleDbSelectCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbInsertCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbUpdateCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbDeleteCommand1;
+        private System.Data.OleDb.OleDbDataAdapter oleDbDataAdapter1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номерDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn видРаботыDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn описаниеРаботыDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Demka_0.1/Demka_0.1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demka_0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+        //Обновление базы данных
+        private void Update()
+        {
+            //обновить содержимое таблиц базы данных
+            oleDbDataAdapter1.Update(dataSet11);
+            //очистить DataSet
+            dataSet11.Clear();
+            //заполнить таблицы в объекте DataSet
+            oleDbDataAdapter1.Fill(dataSet11.Avto);
+        }
+        //Соединение
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oleDbConnection1.Open(); //открыть соединение
+                                     //заполнить таблицы в объекте DataSet
+            oleDbDataAdapter1.Fill(dataSet11.Avto);
+            dataGridView1.Columns[2].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            Update();            
+        }
+       
+        //Добавляем
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //если текстовые поля не пусты,
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                //то создать новую запись в таблице Contacts,
+                DataRow row = dataSet11.Avto.NewRow();
+                //заполнить ее столбцы
+                //Вставляем номер
+                row["Номер"] = textBox1.Text;
+                // Вставляем этаж
+                row["Вид работы"] = textBox2.Text;
+                //Вставляем работника
+                row["Описание работы"] = textBox3.Text;
+                //и добавить запись в таблицу
+                dataSet11.Avto.Rows.Add(row);
+                //обновить содержимое главного окна
+                Update();
+            }
+        }
+        private int clik;
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            clik = e.RowIndex;
+            //отобразить фамилию и имя выбранного человека 
+            //в текстовых полях
+            textBox1.Text = dataSet11.Avto.Rows[clik]["Номер"].ToString();
+            textBox2.Text = dataSet11.Avto.Rows[clik]["Вид работы"].ToString();
+            textBox3.Text = dataSet11.Avto.Rows[clik]["Описание работы"].ToString();
+        }
+        //Удаление строк
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //если выбрана запись для удаления
+            if (dataGridView1.SelectedRows.Count != 0)
+            {
+                if (MessageBox.Show("Вы действительно хотите удалить запись?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        //удалить выбранную строку
+                        dataSet11.Avto.Rows[clik].Delete();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    //обновить БД и ее содержимое на форме
+                    Update();
+                }
+            }
+            else
+                MessageBox.Show("Выберите строку для удаления", "Ошибка");
+        }
+        //Фильтр
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+           
+            //Алгоритм поиска 
+            for (int i = 0; ; )
+            {
+                string name = textBox4.Text;
+                if (name != dataSet11.Avto.Rows[i]["Номер"].ToString())
+                {
+                    i++;
+                }
+                else
+                {
+                    n = i;
+                    break;
+                }
+                
+            }            
+            //изменить фамилию и имя на введенные значения
+            textBox1.Text = dataSet11.Avto.Rows[n]["Номер"].ToString();
+            textBox2.Text = dataSet11.Avto.Rows[n]["Вид работы"].ToString();
+            textBox3.Text = dataSet11.Avto.Rows[n]["Описание работы"].ToString();
+            //сохранить изменения и обновить содержимое формы
+            Update();
+        }
+        //Изменение
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            for (int i = 0; ;)
+            {
+                string name = textBox1.Text;
+                if (name != dataSet11.Avto.Rows[i]["Номер"].ToString())
+                {
+                    i++;
+                }
+                else
+                {
+                    n = i;
+                    break;
+                }
+            }
+            //получить содержимое выбранной строки
+            DataRow row = dataSet11.Avto.Rows[n];
+            //изменить фамилию и имя на введенные значения
+            row["Номер"] = textBox1.Text;
+            row["Вид работы"] = textBox2.Text;
+            row["Описание работы"] = textBox3.Text;
+            //сохранить изменения и обновить содержимое формы
+            Update();
+        }
+    }    
+}
